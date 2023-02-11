9a6a4525509c0a7615b96c48ba81b515d2492216
GyuBin Hwang <spiraldailydev@gmail.com>
2023-02-11 20:45:56 +0900

    add temp test file

---
+++ apps/daily_memo/test/domain/usecase/memo_usecase_test.dart
@@ -0,0 +1,49 @@
+import 'package:apps.daily_memo/domain/model/memo/add_memo_model.dart';
+import 'package:apps.daily_memo/domain/repository_interface/memo/memo_repository.dart';
+import 'package:apps.daily_memo/domain/usecase/memo/memo_usecase.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:mockito/annotations.dart';
+
+class MockMemoRepo extends Mock implements MemoRepository{}
+
+@GenerateMocks(<Type>[MockMemoRepo])
+void main (){
+  late MemoUsecase sut;
+  late MockMemoRepo mockMemoRepo;
+
+  setUp((){
+    mockMemoRepo = MockMemoRepo();
+    sut = MemoUsecase(memoRepository: mockMemoRepo);
+  });
+
+  test('getMemoList', (){
+    // given
+    // when
+    // then
+  });
+  test('getMemoById', (){
+    // given
+    // when
+    // then
+  });
+  test('addMemo', () async {
+    // given
+
+    // when
+    AddMemoModel temp = await sut.addMemo(AddMemoModel("test title", "test content"));
+
+    // then
+    expect("test title", temp.title);
+  });
+  test('modifyMemo', (){
+    // given
+    // when
+    // then
+  });
+  test('deleteMemo', (){
+    // given
+    // when
+    // then
+  });
+}
---
+++ apps/daily_memo/test/presentation/view_model/home/home_viewmodel_test.dart
@@ -0,0 +1,8 @@
+import 'package:apps.daily_memo/presentation/view_model/home/home_viewmodel.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '';
+
+void main (){
+
+}
